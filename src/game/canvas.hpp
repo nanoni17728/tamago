1fafc48f7a5aa30073f9f47b6393ecd44a7127a4
nanoni17728 <nanoni@riseup.net>
2023-09-18 10:48:30 +0200

    Idiot CPP

---
+++ src/game/canvas.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#define SCREEN_WIDTH 10
+#define SCREEN_HEIGHT 10
+
+int canvas[SCREEN_WIDTH][SCREEN_HEIGHT];
--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,6 +1,12 @@
+#include "canvas.hpp"
 #include <iostream>
 
 int main(void) {
-  std::cout << "Hello World!";
+  
+  canvas[0][0] = 10;
+
+  std::cout << canvas[0][0] << "\n";
+
   return EXIT_SUCCESS;
+
 }
